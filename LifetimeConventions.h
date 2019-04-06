6eda897757272a569eaeb72e275655069aa83a58
Linda Cremonesi <l.cremonesi@ucl.ac.uk>
2019-04-06 11:37:44 +0200

    Updated PrM2 distances

--- LifetimeConventions.h
+++ LifetimeConventions.h
@@ -14,7 +14,7 @@ const std::string whereIsMyPrMdata="/Users/linda/DUNE/exampleDataFromDigitiser/"
 // First Purity Monitor machined at UCL (PrM1)
 Double_t PrM1distance[3] = {0.01823, 0.16424, 0.00985};
 // ICARUS Purity Monitor refurbished (PrM2)
-Double_t PrM2distance[3] = {0.016, 0.15, 0.085};
+Double_t PrM2distance[3] = {0.018, 0.15, 0.01};
 
 
 // pre-amplifiers tau electronics for PrM1 {K, A} and relative gain A/K TODO: APPLY CALIBRATION AND CHECK THESE NUMBERS !
