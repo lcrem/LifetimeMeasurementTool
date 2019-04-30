17bca665742bfe73227f889a6b6af54d783a15a1
Linda Cremonesi <l.cremonesi@ucl.ac.uk>
2019-04-30 18:21:57 +0100

    Update gains from latest evaluation, and also decay times are all 270us now

--- LifetimeConventions.h
+++ LifetimeConventions.h
@@ -18,8 +18,8 @@ Double_t PrM2distance[3] = {0.018, 0.15, 0.01};
 
 
 // pre-amplifiers tau electronics for PrM1 {K, A} and relative gain A/K , Preamp B on K, Preamp A on A
-Double_t PrM1preamp[3] = {269.*1e-6, 101.*1e-6, 1.3561329};
+Double_t PrM1preamp[3] = {270.*1e-6, 270.*1e-6, 1.1};
 // pre-amplifiers tau electronics for PrM2 {K, A} and relative gain A/K , Preamp D on K, Preamp C on A
-Double_t PrM2preamp[3] = {262.*1e-6, 101.*1e-6, 1.1750635};
+Double_t PrM2preamp[3] = {270.*1e-6, 270.*1e-6, 1.1};
 
 #endif //LIFETIMECONVENTIONS_H
